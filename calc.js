fb4ee86d43d6def4202e3d6331919d364d26bef3
Group035 <Group035@infoway.com>
2022-11-09 14:53:53 +0530

    File commit

---
+++ calc.js
@@ -0,0 +1,33 @@
+function calcu(){
+    var num1=parseInt(document.getElementById("num1").value);
+    var num2=parseInt(document.getElementById("num2").value);
+    var ops=document.getElementsByName("cal");
+    //console.log(ops);
+    //console.log(ops[0].checked);
+    var res;
+    var ops_val;
+    for(let i = 0;i<ops.length;i++){
+        //console.log(ops[i].checked);
+        if(ops[i].checked){            
+            console.log(ops[i].value);
+            ops_val = ops[i].value;
+        }
+    }
+    if(ops_val=="add")
+    {
+        res = num1 + num2;
+    }
+    if(ops_val=="sub")
+    {
+        res = num1 - num2;
+    }
+    if(ops_val=="mul")
+    {
+        res = num1 * num2;
+    }
+    if(ops_val=="div")
+    {
+        res = num1 / num2;
+    }
+    document.getElementById("res").innerText="Result: "+res;
+}
